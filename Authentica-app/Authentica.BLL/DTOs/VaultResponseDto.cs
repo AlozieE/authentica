5e2d0df6ae49be77105b34e9db82ddfadbc3098a
alozieekentason <577854@student.fontys.nl>
2026-06-09 13:36:41 +0200

    Remove unused VaultResponseDto

--- Authentica-app/Authentica.BLL/DTOs/VaultResponseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Authentica.BLL.DTOs
-{
-    public class VaultResponseDto
-    {
-        public int VaultId { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; }
-    }
-}
